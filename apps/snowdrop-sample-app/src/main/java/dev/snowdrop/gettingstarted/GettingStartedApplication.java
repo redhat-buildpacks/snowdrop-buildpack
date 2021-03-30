0aedf2ecd31d0875be232c52f4b1457988140578
Charles Moulliard <ch007m@gmail.com>
2021-03-30 16:28:26 +0200

    Add spring boot example

---
+++ apps/snowdrop-sample-app/src/main/java/dev/snowdrop/gettingstarted/GettingStartedApplication.java
@@ -0,0 +1,11 @@
+package dev.snowdrop.gettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedApplication.class, args);
+	}
+}
