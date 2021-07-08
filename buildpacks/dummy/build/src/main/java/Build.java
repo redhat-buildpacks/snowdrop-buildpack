fac8526e4775784400d3b6a2ae7c13198a92d360
Charles Moulliard <ch007m@gmail.com>
2021-07-08 17:21:49 +0200

    Create a build and a detect project

--- buildpacks/dummy/build/src/main/java/Build.java
+++ buildpacks/dummy/build/src/main/java/Build.java
@@ -2,40 +2,43 @@ package dev.snowdrop.buildpack;
 
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
-import io.quarkus.runtime.annotations.CommandLineArguments;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import picocli.CommandLine;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-@QuarkusMain(name = "detect")
-public class Detect implements QuarkusApplication {
+@QuarkusMain
+public class Build implements QuarkusApplication {
 
     public static void main(String[] argv) throws Exception {
-        Quarkus.run(Detect.class, argv);
+        Quarkus.run(Build.class, argv);
     }
 
-    @CommandLine.Command(name = "detect")
-    public int detect(
+    @CommandLine.Command(name = "build")
+    public void build(
         @CommandLine.Parameters(
-                index = "0",
+                    index = "0",
+                    description = "A directory that may contain subdirectories representing each layer created by the buildpack in the final image or build cache",
+                    paramLabel = "LAYERS_DIR") String LAYERS_DIR,
+        @CommandLine.Parameters(
+                index = "1",
                 description = "A directory containing platform provided configuration, such as environment variables",
                 paramLabel = "PLATFORM_DIR") String PLATFORM_DIR,
         @CommandLine.Parameters(
-                index = "1",
+                index = "2",
                 description = "A path to a file containing the Build Plan",
                 paramLabel = "BUILD_PLAN") String BUILD_PLAN
         ) throws Exception {
-            System.out.println("Detect called");
+            System.out.println("Build called");
+            System.out.println("Layers dir: " + LAYERS_DIR);
             System.out.println("Build plan: " + BUILD_PLAN);
             System.out.println("Platform dir: " + PLATFORM_DIR);
-            return 0;
     }
 
     @Override
     public int run(String... args) throws Exception {
-        int exitCode = new CommandLine(CommandLine.getCommandMethods(Detect.class,"detect").get(0)).execute(args);
+        int exitCode = new CommandLine(CommandLine.getCommandMethods(Build.class,"build").get(0)).execute(args);
         return exitCode;
     }
 }
--- buildpacks/dummy/detect/src/main/java/Detect.java
+++ buildpacks/dummy/detect/src/main/java/Detect.java
@@ -8,37 +8,33 @@ import picocli.CommandLine;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-@QuarkusMain(name = "build")
-public class Build implements QuarkusApplication {
+@QuarkusMain
+public class Detect implements QuarkusApplication {
 
     public static void main(String[] argv) throws Exception {
-        Quarkus.run(Build.class, argv);
+        Quarkus.run(Detect.class, argv);
     }
 
-    @CommandLine.Command(name = "build")
-    public void build(
+    @CommandLine.Command(name = "detect")
+    public int detect(
         @CommandLine.Parameters(
-                    index = "0",
-                    description = "A directory that may contain subdirectories representing each layer created by the buildpack in the final image or build cache",
-                    paramLabel = "LAYERS_DIR") String LAYERS_DIR,
-        @CommandLine.Parameters(
-                index = "1",
+                index = "0",
                 description = "A directory containing platform provided configuration, such as environment variables",
                 paramLabel = "PLATFORM_DIR") String PLATFORM_DIR,
         @CommandLine.Parameters(
-                index = "2",
+                index = "1",
                 description = "A path to a file containing the Build Plan",
                 paramLabel = "BUILD_PLAN") String BUILD_PLAN
         ) throws Exception {
-            System.out.println("Build called");
-            System.out.println("Layers dir: " + LAYERS_DIR);
+            System.out.println("Detect called");
             System.out.println("Build plan: " + BUILD_PLAN);
             System.out.println("Platform dir: " + PLATFORM_DIR);
+            return 0;
     }
 
     @Override
     public int run(String... args) throws Exception {
-        int exitCode = new CommandLine(CommandLine.getCommandMethods(Build.class,"build").get(0)).execute(args);
+        int exitCode = new CommandLine(CommandLine.getCommandMethods(Detect.class,"detect").get(0)).execute(args);
         return exitCode;
     }
 }
