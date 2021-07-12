b5292d89c7c17d1bbedc19687541b3a03505b002
Charles Moulliard <ch007m@gmail.com>
2021-07-12 11:43:21 +0200

    Log stdout and stderr

--- buildpacks/dummy/main/src/main/java/dev/snowdrop/buildpack/utils/ProcessHandler.java
+++ buildpacks/dummy/main/src/main/java/dev/snowdrop/buildpack/utils/ProcessHandler.java
@@ -1,17 +1,35 @@
 package dev.snowdrop.buildpack.utils;
 
+import org.jboss.logging.Logger;
+
 import java.io.IOException;
 import java.util.Optional;
 import java.util.Scanner;
 
 public class ProcessHandler {
+    static final org.jboss.logging.Logger LOG = Logger.getLogger(ProcessHandler.class);
 
-    public static void runtimeCmd(String cmd) throws IOException {
+    public static void runtimeCmd(String cmd) throws IOException, InterruptedException {
         java.lang.Process p = Runtime.getRuntime().exec(new String[]{"bash", "-c", cmd});
+
+        // Log stdout
         Scanner s = new Scanner(p.getInputStream());
+        LOG.infof("Stdout of the command executed: %s", cmd);
         while(s.hasNext()) {
-            System.out.println(s.nextLine());
+            LOG.info(s.nextLine());
         };
+
+        // TODO: Review the code to LOG if only stderr is not empty
+        // Log stderr
+        s = new Scanner(p.getErrorStream());
+        LOG.infof("Stderr of the command executed: %s", cmd);
+        while(s.hasNext()) {
+            LOG.info(s.nextLine());
+        };
+
+        // Exit code
+        int exitVal = p.waitFor();
+        LOG.info("Exited with error code " + exitVal);
     }
 
     public static Scanner runtimeCmdScanner(String cmd) throws IOException {
