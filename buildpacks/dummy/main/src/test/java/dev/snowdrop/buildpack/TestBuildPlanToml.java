3044694f0ae038cbae3df9a496ede6ad808beb19
Charles Moulliard <ch007m@gmail.com>
2021-07-12 15:49:05 +0200

    Add method to test to write

--- buildpacks/dummy/main/src/test/java/dev/snowdrop/buildpack/TestBuildPlanToml.java
+++ buildpacks/dummy/main/src/test/java/dev/snowdrop/buildpack/TestBuildPlanToml.java
@@ -1,13 +1,28 @@
 package dev.snowdrop.buildpack;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.toml.TomlMapper;
 import dev.snowdrop.buildpack.model.BuildPlan;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TestBuildPlanToml {
+
+    @Test
+    public void writeBuildPlanTest() throws JsonProcessingException {
+        String result = "requires = ''\n" +
+                "provides = ''\n";
+        BuildPlan bp = new BuildPlan();
+
+        TomlMapper mapper = new TomlMapper();
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        String toml = mapper.writeValueAsString(bp);
+        assertNotNull(toml);
+        assertEquals(result,toml);
+    }
 
     @Test
     public void readBuildPlanTest() throws JsonProcessingException {
