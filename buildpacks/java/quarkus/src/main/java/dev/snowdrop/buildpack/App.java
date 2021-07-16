3d81bdbebb7d88f6b334cbd664a84ce1a60eb5f1
Charles Moulliard <ch007m@gmail.com>
2021-07-16 15:00:14 +0200

    Add try/catch

--- buildpacks/java/quarkus/src/main/java/dev/snowdrop/buildpack/App.java
+++ buildpacks/java/quarkus/src/main/java/dev/snowdrop/buildpack/App.java
@@ -1,6 +1,5 @@
 package dev.snowdrop.buildpack;
 
-import dev.snowdrop.buildpack.utils.ProcessHandler;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
@@ -8,7 +7,7 @@ import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 
-import static dev.snowdrop.buildpack.utils.EnvVars.printAllEnv;
+import static dev.snowdrop.buildpack.utils.ProcessHandler.commandProcessed;
 import static java.lang.String.join;
 
 @ApplicationScoped
@@ -16,37 +15,42 @@ import static java.lang.String.join;
 public class App implements QuarkusApplication {
     static final Logger LOG = Logger.getLogger(App.class);
 
-    public static void main(String[] argv) throws Exception {
+    public static void main(String[] argv) {
         Quarkus.run(App.class, argv);
     }
 
     @Override
-    public int run(String... args) throws Exception {
-        LOG.info("## App called ...");
-        String BP_CMD = System.getenv("BP_CMD");
-        if (BP_CMD == null) {
-            BP_CMD = "ps -eo command | grep '/bin/*'";
-        }
-        LOG.info("BP_CMD :" + BP_CMD);
-        LOG.infof("## Arguments passed: %s",join(" ", args));
-        LOG.info("## Env Var ...");
-        printAllEnv();
+    public int run(String... args) {
+        try {
+            LOG.info("## App called ...");
+            String BP_CMD = System.getenv("BP_CMD");
+            if (BP_CMD == null) {
+                BP_CMD = "ps -eo command | grep '/bin/*'";
+            }
+            LOG.info("BP_CMD :" + BP_CMD);
+            LOG.infof("## Arguments passed: %s",join(" ", args));
+            LOG.info("## Env Var ...");
+            //printAllEnv();
 
-        LOG.info("## Check the name of the program called by the lifecycle creator");
-        switch (ProcessHandler.commandProcessed(BP_CMD)) {
-            case "detect":
-                LOG.info("## Command called is /bin/detect");
-                Detect d = new Detect(args);
-                return d.call();
-            case "build":
-                LOG.info("## Command called is /bin/build");
-                Build b = new Build(args);
-                return b.call();
-            case "":
-                LOG.error("## Unsupported command called !");
-                return 1;
+            LOG.info("## Check the name of the program called by the lifecycle creator");
+            switch (commandProcessed(BP_CMD)) {
+                case "detect":
+                    LOG.info("## Command called is /bin/detect");
+                    Detect d = new Detect(args);
+                    return d.call();
+                case "build":
+                    LOG.info("## Command called is /bin/build");
+                    Build b = new Build(args);
+                    return b.call();
+                case "":
+                    LOG.error("## Unsupported command called !");
+                    return 1;
+            }
+            return 0;
+        } catch(Exception ex) {
+            LOG.error("## Error happened during the call to the Application ");
+            LOG.error(ex);
+            return 1;
         }
-
-        return 0;
     }
 }
